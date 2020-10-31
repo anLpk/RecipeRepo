048e22816d8d5daaecb73e55f23b0e6fd57c1844
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-10-31 23:31:22 +1100

    Update cookbook.rb

--- cookbook.rb
+++ cookbook.rb
@@ -1,36 +1,65 @@
-require 'csv'
+require "csv"
+require "pry-byebug"
+require_relative "recipe"
 
 class Cookbook
   def initialize(csv_file_path)
-    @recipes = []
     @csv_file_path = csv_file_path
-    populate_recipes
-  end
-
-  def populate_recipes
-    CSV.foreach(@csv_file_path) do |row|
-      @recipes << Recipe.new(row[0], row[1])
-    end
+    @recipes = []
+    load_csv
   end
 
   def all
-    @recipes
+    return @recipes
   end
 
   def add_recipe(recipe)
     @recipes << recipe
-    update_csv
+    save_csv
   end
 
   def remove_recipe(recipe_index)
     @recipes.delete_at(recipe_index)
-    update_csv
+    save_csv
   end
 
-  def update_csv
+  def mark_recipe(index_to_mark)
+    recipe_to_mark = @recipes[index_to_mark]
+    recipe_to_mark.done!
+    save_csv
+  end
+
+  private
+
+  def load_csv
+    csv_options = { headers: true, header_converters: :symbol }
+    CSV.foreach(@csv_file_path, csv_options ) do |row|
+
+      #row behave like a hash row[:name] / row[:description]
+      #normal if/else
+      # if row[:done] == "true"
+      #   row[:done] = true
+      # elsif row[:done] == "false"
+      #   row[:done] = false
+      # end
+      #
+
+      #ternary
+      #                #or true or false
+      # row[:done] = (row[:done] == "true") ? true : false
+
+      #shorter way
+      row[:done] = (row[:done] == "true")
+      recipe = Recipe.new(row)
+      @recipes << recipe
+    end
+  end
+
+  def save_csv
     CSV.open(@csv_file_path, 'wb') do |csv|
-      @recipes.each do |recip|
-        csv << [recip.name, recip.description]
+      csv << ["name", "description", "rating", "done", "prep_time"]
+      @recipes.each do |recipe|
+        csv << [recipe.name, recipe.description, recipe.rating, recipe.done?, recipe.prep_time]
       end
     end
   end
